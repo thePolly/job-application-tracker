1fb5efb5bff6a85250aeb80c5e7c69180ab3d40a
Polina Katkova <lin997@yandex.ru>
2026-05-15 22:39:47 +0200

    deleted files

--- src/main/java/com/polina/job_application_tracker/repository/JobApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.polina.job_application_tracker.repository;
-
-import com.polina.job_application_tracker.entity.JobApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
-}
