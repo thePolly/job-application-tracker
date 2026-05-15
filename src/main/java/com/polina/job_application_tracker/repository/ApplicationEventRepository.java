f1127cbd9ec1e60246c591741d451b4567715cef
Polina Katkova <lin997@yandex.ru>
2026-05-15 21:53:43 +0200

    added statistics endpoint

--- src/main/java/com/polina/job_application_tracker/repository/ApplicationEventRepository.java
+++ src/main/java/com/polina/job_application_tracker/repository/ApplicationEventRepository.java
@@ -1,11 +1,18 @@
 package com.polina.job_application_tracker.repository;
 
 import com.polina.job_application_tracker.entity.ApplicationEvent;
+import com.polina.job_application_tracker.enums.ApplicationEventType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 public interface ApplicationEventRepository extends JpaRepository<ApplicationEvent, Long> {
 
     List<ApplicationEvent> findByJobApplicationId(Long jobApplicationId);
+
+    long countByTypeAndEventDateBetween(ApplicationEventType type, LocalDate startDate, LocalDate endDate);
+
+    long countByTypeInAndEventDateBetween(Collection<ApplicationEventType> types, LocalDate startDate, LocalDate endDate);
 }
